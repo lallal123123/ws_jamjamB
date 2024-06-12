c39e28c6b5291f98de61318f743356fb9cda5baa
jun <wnsgud0202@naver.com>
2024-06-12 22:17:00 +0900

    서버구동

---
+++ viewServer/src/main/java/com/project/viewServer/ViewServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.viewServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewServerApplication.class, args);
+	}
+
+}
